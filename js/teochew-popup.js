3854b087c8dd56ea760b4cd61600c3a78ad1336b
Paul Ron La <paul.la@utexas.edu>
2019-08-16 23:59:13 -0500

    daily update

--- js/teochew-popup.js
+++ js/teochew-popup.js
@@ -1,7 +1,14 @@
 'use strict';
 
-class TeochewPopup extends HTMLElement {
-    constructor() {
-        super();
-    }
+if (customElements) {
+    customElements.define('teochew-popup', 
+        class TeochewPopup extends HTMLElement {
+            constructor() {
+                super();
+
+                const shadowRoot = this.attachShadow({mode: 'open'});
+                const sheet = new CSSStyleSheet();
+            }
+        }
+    );
 }
